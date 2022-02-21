8e935c81eb782cce23016283eb6861a41d544219
Irfan Shah <Irfanshah676@gmail.com>
2022-02-21 18:23:48 +0530

    IS-new image add

--- github-practice/src/Components/IRFANSHAH/IrfanShah.jsx
+++ github-practice/src/Components/IRFANSHAH/IrfanShah.jsx
@@ -4,6 +4,7 @@ const IrfanShah = () => {
     return (
         <div>
             <h1>I am Irfan Shah</h1>
+            <img src="https://res.cloudinary.com/twenty20/private_images/t_standard-fit/photosp/876b471e-7869-4370-a91c-3e1a745efdbb/stock-photo-nature-forest-holding-protection-agriculture-green-hand-life-human-876b471e-7869-4370-a91c-3e1a745efdbb.jpg" alt="Image" />
         </div>
     );
 };
